fcc77aa0608e00efd858b8d4ed9b52b2d6a6242c
Myles Reid <myles@reidsmailbox.com>
2025-04-03 13:42:50 -0500

    Character model filled

--- TomeOfHolding.Models/Character.cs
+++ TomeOfHolding.Models/Character.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TomeOfHolding.Models
-{
-    public class Character
-    {
-    }
+﻿namespace TomeOfHolding.Models {
+	public class Character {
+		public int CharacterId { get; set; }
+		public string Name { get; set; }
+		public string Class { get; set; }
+		public string Role { get; set; }
+		public int Level { get; set; }
+		public string Race { get; set; }
+		public string Description { get; set; }
+		public int Currency { get; set; }
+		public int CampaignId { get; set; }
+		public Campaign Campaign { get; set; }
+		public int CharacterSheetId { get; set; }
+		public CharacterSheet CharacterSheet { get; set; }
+	}
 }
--- TomeOfHolding.Models/Session.cs
+++ TomeOfHolding.Models/Session.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TomeOfHolding.Models
-{
-    public class Session
-    {
-    }
+﻿namespace TomeOfHolding.Models {
+	public class Session {
+	}
 }
