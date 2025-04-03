acaefd4b3f911793672f2008ea8409566b841258
Myles Reid <myles@reidsmailbox.com>
2025-04-03 13:55:46 -0500

    Started Context, Added Default connection to Appsettings.json

--- TomeOfHolding.DAL/TomeOfHoldingContext.cs
+++ TomeOfHolding.DAL/TomeOfHoldingContext.cs
@@ -1,7 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TomeOfHolding.Models;
 
 namespace TomeOfHolding.DAL {
+	
 	public class TomeOfHoldingContext : DbContext {
+		public DbSet<Campaign> Campaigns { get; set; }
+		public DbSet<Session> Sessions { get; set; }
+		public DbSet<Character> Characters { get; set; }
+		public DbSet<CharacterSheet> CharacterSheets { get; set; }
+		public DbSet<Player> Players { get; set; }
+		public DbSet<Note> Notes { get; set; }
+		public DbSet<Encounter> Encounters { get; set; }
 
 		public TomeOfHoldingContext(DbContextOptions<TomeOfHoldingContext> options) : base(options) {
 		}
