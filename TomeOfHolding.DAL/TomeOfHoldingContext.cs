91f510d30c05ea49a4569e7ec76518aede5ffadb
Myles Reid <myles@reidsmailbox.com>
2025-04-03 13:39:07 -0500

    Inital setup. Proj. references and models layed out

---
+++ TomeOfHolding.DAL/TomeOfHoldingContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TomeOfHolding.DAL {
+	public class TomeOfHoldingContext : DbContext {
+
+		public TomeOfHoldingContext(DbContextOptions<TomeOfHoldingContext> options) : base(options) {
+		}
+	}
+}
