05ad0a82e8879149a666c9f28f5a446c0cee6957
Myles Reid <myles@reidsmailbox.com>
2025-04-03 13:46:28 -0500

    Note, player and Session Model filled

--- TomeOfHolding.Models/Session.cs
+++ TomeOfHolding.Models/Session.cs
@@ -1,4 +1,14 @@
-﻿namespace TomeOfHolding.Models {
+﻿using System.Diagnostics.Contracts;
+
+namespace TomeOfHolding.Models {
 	public class Session {
+		public int SessionId { get; set; }
+		public DateOnly Date { get; set; }
+		public string Summary { get; set; }
+		public ICollection<Encounter> Encounters { get; set; } = new List<Encounter>();
+		public ICollection<Note> Notes { get; set; } = new List<Note>();
+		public int CampaignId { get; set; }
+		public Campaign Campaign { get; set; } = new Campaign();
+
 	}
 }
