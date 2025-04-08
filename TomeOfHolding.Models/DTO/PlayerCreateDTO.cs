8d9ee63608502393f3ff3c7bb4f2a0f67b7652a2
Myles Reid <myles@reidsmailbox.com>
2025-04-08 13:03:17 -0500

    Player create DTO

--- TomeOfHolding.Models/DTO/PlayerCreateDTO.cs
+++ TomeOfHolding.Models/DTO/PlayerCreateDTO.cs
@@ -1,4 +1,10 @@
 ﻿namespace TomeOfHolding.Models.DTO {
 	public class PlayerCreateDTO {
+		public string Name { get; set; }
+		public List<DayOfWeek> AvailableDays { get; set; } = new List<DayOfWeek>();
+		public string Role { get; set; }
+		public List<int> CampaignIDs { get; set; }
+		public List<int> CharacterIDs { get; set; }
+		public List<int> NoteIDs { get; set; }
 	}
 }
