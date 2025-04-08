df24e195df1b5e7e615ae10ca46d8dde57fafbe8
Myles Reid <myles@reidsmailbox.com>
2025-04-08 12:57:04 -0500

    Campaign create DTO

--- TomeOfHolding.Models/DTO/CampCreateDTO.cs
+++ TomeOfHolding.Models/DTO/CampCreateDTO.cs
@@ -1,4 +1,11 @@
 ﻿namespace TomeOfHolding.Models.DTO {
 	public class CampCreateDTO {
+		public string Title { get; set; }
+		public int GM_ID { get; set; }
+		public string Description { get; set; }
+		public List<int> SessionIds { get; set; }
+		public List<int> PlayerIds { get; set; }
+		public List<int> CharacterIds { get; set; }
+
 	}
 }
