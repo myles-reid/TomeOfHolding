b7fc3315ab98cac59c392abc54c53d34824b37e9
Samuel Reutcky <sreutcky@gmail.com>
2025-04-10 13:23:31 -0500

    Deleted classes

--- TomeOfHolding.Models/DTO/NoteCreateDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TomeOfHolding.Models.DTO {
-	public class NoteCreateDTO {
-		public int SessionId { get; set; }
-		public int PlayerId { get; set; }
-		public string Notes { get; set; }
-	}
-}
